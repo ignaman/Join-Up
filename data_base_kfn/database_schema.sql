edb1962291f57e2fb58d9d057b02993556072b3e
ignaman <ignabogdanmihai@gmail.com>
2024-11-08 23:39:52 +0200

    removed comments

--- data_base_kfn/database_schema.sql
+++ data_base_kfn/database_schema.sql
@@ -4,7 +4,6 @@ CREATE TABLE Region (
     Name CHAR(100)
 );
 
--- Create User table
 CREATE TABLE User (
     UserID INT PRIMARY KEY,
     Name CHAR(100),
@@ -13,20 +12,17 @@ CREATE TABLE User (
     FOREIGN KEY (RegionID) REFERENCES Region(RegionID)
 );
 
--- Create Event table
 CREATE TABLE Event (
     EventID INT PRIMARY KEY,
     EventName CHAR(100),
     Venue CHAR(100)
 );
 
--- Create Interests table
 CREATE TABLE Interests (
     InterestID INT PRIMARY KEY,
     InterestName CHAR(100)
 );
 
--- Create UserEvents junction table for many-to-many relationship between User and Event
 CREATE TABLE UserEvents (
     UserID INT,
     EventID INT,
@@ -35,7 +31,6 @@ CREATE TABLE UserEvents (
     FOREIGN KEY (EventID) REFERENCES Event(EventID)
 );
 
--- Create UserInterests junction table for many-to-many relationship between User and Interests
 CREATE TABLE UserInterests (
     UserID INT,
     InterestID INT,
